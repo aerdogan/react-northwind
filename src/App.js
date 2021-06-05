a9abeddc9c3f8cf27808997f0d05948c2979b407
aerdogan <tuxposta@gmail.com>
2021-06-06 01:00:40 +0300

    get product data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import Dashboard from './layouts/Dashboard';
+import { Container } from 'semantic-ui-react';
+import Navi from './layouts/Navi';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard></Dashboard>
+      <Container className="main">
+        <Navi></Navi>
+        <Dashboard></Dashboard>
+      </Container>
     </div>
   );
 }
